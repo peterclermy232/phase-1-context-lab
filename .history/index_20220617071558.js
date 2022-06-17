7b7e65e49b076fdf648a1bd3904f6b9e78b1080e
peterclermy <peterclermy232@gmail.com>
2022-06-17 07:19:05 +0300

    commit tree 514f8b7630a5fdf3fc4e505f0ea8bc6d406889cc

---
+++ .history/index_20220617071558.js
@@ -0,0 +1,23 @@
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
+
